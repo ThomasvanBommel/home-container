8da3d295d91826da61f6036bedc7354a5023f283
ThomasvanBommel <thomas@vanbommel.ca>
2021-11-11 20:46:45 -0400

    Changed .send to .sendFile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(res.sendFile(path.join(__dirname, "html/index.html")));
+    res.sendFile(res.sendFile(path.join(__dirname, "html/index.html")));
 });
 
 app.listen(process.env.PORT, () => {
