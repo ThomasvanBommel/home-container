9f9babe5e87c4a1f6a822c2eb1198d391c0eab17
ThomasvanBommel <thomas@vanbommel.ca>
2021-11-15 11:04:23 -0400

    Updated port logic

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const port = process.env.PORT | 8000;
+
 app.use(express.static("static"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "html/index.html"));
 });
 
-app.listen(process.env.PORT | 8000, () => {
-    console.log("Listening...");
+app.listen(port, () => {
+    console.log(`Listening port ${ port }...`);
 });
