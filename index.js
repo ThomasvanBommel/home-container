01ed6a16868344bf759253fda53a4f4f5e3720c9
ThomasvanBommel <thomas@vanbommel.ca>
2021-11-15 09:52:32 -0400

    Updated content

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static("static"));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "html/index.html"));
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT | 8000, () => {
     console.log("Listening...");
 });
