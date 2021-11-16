8c32a136bb91c35e41fe86066233a3695cd9d478
ThomasvanBommel <thomas@vanbommel.ca>
2021-11-15 22:54:59 -0400

    Added logging middleware

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+// Log request middleware
+app.use((req, res, next) => {
+    console.log(req.ip, "(", req.url, ")");
+    next();
+});
+
+// Serve static files middleware
 app.use(express.static("static"));
 
+// Serve homepage
 app.get("/", (req, res) => {
-    console.log(req.ip, "(", req.url, ")");
     res.sendFile(path.join(__dirname, "html/index.html"));
 });
 
+// Start listening
 app.listen(port, () => {
     console.log(`Listening port ${ port }...`);
 });
