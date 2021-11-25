155e7c10e0adbf4cdb07cda20ae15dd033c917ba
ThomasvanBommel <thomas@vanbommel.ca>
2021-11-24 22:28:12 -0400

    Updated test script to remove containers tagged '<none>'

--- test.sh
+++ test.sh
@@ -3,4 +3,11 @@
 set -e
 
 docker build --tag home .
+
+IMAGES=$(docker images | grep "<none>" | awk '{ print $3 }')
+
+if [ ! -z "$IMAGES" ]; then
+    docker rmi -f $IMAGES
+fi
+
 docker run --rm -ti -v $(pwd):/opt/project -p 9000:8080 home
